f1f87d68ec9f114cfd791658d69fdc82ea8f0c34
gusehdg <gusehdg@DESKTOP-UESE1ST>
2018-08-03 02:15:33 +0900

    hi

--- src/helloword/HelloWorld.java
+++ src/helloword/HelloWorld.java
@@ -3,8 +3,8 @@ package helloword;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("안녕");
+		System.out.println("hi");
 	}
 
 }
